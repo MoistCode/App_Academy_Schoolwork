0897ab6ea79dd23638237f84d1734d4a8420b96c
Tommy Pham <phamtommylong1991@gmail.com>
2018-02-27 12:02:51 -0800

    Some inserts into the table

--- Projects/W3D2/Question_Pair/import_db.sql
+++ Projects/W3D2/Question_Pair/import_db.sql
@@ -39,3 +39,51 @@ CREATE TABLE question_likes (
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Timmy', 'Turner'),
+  ('Lirik', 'Kun'),
+  ('Tommy', 'Pham'),
+  ('Gayle', 'Laakmann'),
+  ('Starbucks', 'Coffee'),
+  ('La', 'Croix'),
+  ('Gamma', 'Helm'),
+  ('Moist', 'Code'),
+  ('Johnson', 'Vlissides'),
+  ('Donald', 'Knuth');
+
+INSERT INTO
+  questions (title, body, user_id)
+VALUES
+  ('Lost Cow', 'How do I end up losing my cow on a Wednesday?', (SELECT id FROM users WHERE fname = 'Gayle' AND lname = 'Laakmann') )
+  ('Starbucks', 'When should I drink starbucks?', (SELECT id FROM users WHERE fname = 'Moist' AND lname = 'Code') )
+  ('Moist Toilet Seat', 'Why am I so bad at aiming?', (SELECT id FROM users WHERE fname = 'Timmy' AND lname = 'Turner') )
+  ('Awesomeness', 'Why am I so awesome?', (SELECT id FROM users WHERE fname = 'Gamma' AND lname = 'Helm') )
+  ('Sequal', 'Why do people call it sequal instead of just the noise SQL?', (SELECT id FROM users WHERE fname = 'Gayle' AND lname = 'Laakmann') )
+  ('Concrete Mathematics', 'Why did I choose to co-write this book?', (SELECT id FROM users WHERE fname = 'Donald' AND lname = 'Knuth') )
+  ('', '', (SELECT id FROM users WHERE fname = 'Gayle' AND lname = 'Laakmann') )
+  ('', '', (SELECT id FROM users WHERE fname = 'Timmy' AND lname = 'Turner') )
+  ('', '', (SELECT id FROM users WHERE fname = 'Gayle' AND lname = 'Laakmann') )
+  ('', '', (SELECT id FROM users WHERE fname = 'La' AND lname = 'Croix') )
+
+
+
+
+
+INSERT INTO
+
+VALUES
+
+INSERT INTO
+
+VALUES
+
+INSERT INTO
+
+VALUES
+
+INSERT INTO
+
+VALUES
