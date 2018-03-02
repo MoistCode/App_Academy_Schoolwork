2a482fc276a699975cb3a9eb8d210c1ef98a1f15
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-02 13:26:41 -0800

    Start of SQL objects

--- Projects/W3D5/Active_Record_Lite/lib/01_sql_object.rb
+++ Projects/W3D5/Active_Record_Lite/lib/01_sql_object.rb
@@ -2,21 +2,33 @@ require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
-
+require 'byebug'
 class SQLObject
+
   def self.columns
-    # ...
+    # Established a connection.
+    # For some reason, the question mark didn't work here?
+    # Table name here is nil so I added cats
+    
+    DBConnection.instance.execute(<<-SQL)
+    SELECT
+      *
+    FROM
+      cats
+
+    SQL
   end
 
   def self.finalize!
+
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= 'cats'
   end
 
   def self.all
@@ -32,7 +44,7 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+
   end
 
   def attributes
