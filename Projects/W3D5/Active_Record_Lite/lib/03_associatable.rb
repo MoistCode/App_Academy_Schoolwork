7f441e82a576a80578141d35cffbae9fef816d01
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-02 17:05:39 -0800

    End of day

--- Projects/W3D5/Active_Record_Lite/lib/03_associatable.rb
+++ Projects/W3D5/Active_Record_Lite/lib/03_associatable.rb
@@ -1,43 +1,54 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
-
-# Phase IIIa
-class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
-
-  def model_class
-    
-  end
-
-  def table_name
-    # ...
-  end
-end
-
-class BelongsToOptions < AssocOptions
-  def initialize(name, options = {})
-    # ...
-  end
-end
-
-class HasManyOptions < AssocOptions
-  def initialize(name, self_class_name, options = {})
-    # ...
-  end
-end
-
 module Associatable
-  # Phase IIIb
+
+  def defaulter(name)
+    define_method("class_name") do
+      name.to_s.capitalize
+    end
+
+    define_method("foreign_key") do
+      "#{name}_id".to_sym
+    end
+
+    define_method("primary_key") do
+      :id
+    end
+  end
+
+  def defaulter2(name, self_class_name)
+    define_method('foreign_key') do
+      "#{self_class_name.downcase}_id".to_sym
+    end
+
+    define_method('class_name') do
+      "#{name.singularize.capitalize}"
+    end
+
+    define_method('primary_key') do
+      :id
+    end
+  end
+
+  def overrider(options)
+    options.each do |k, v|
+      define_method(k.to_s) do
+        v
+      end
+    end
+  end
+
   def belongs_to(name, options = {})
-    # ...
+    defaulter(name)
+    overrider(options)
+
+    define_method("#{name}") do
+      self.class_name.constantize.find(self.foreign_key)
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+
   end
 
   def assoc_options
@@ -45,6 +56,53 @@ module Associatable
   end
 end
 
+# Phase IIIa
+class AssocOptions
+  extend Associatable
+
+  attr_accessor(
+    :foreign_key,
+    :class_name,
+    :primary_key
+  )
+
+  def model_class
+    self.class_name.constantize
+  end
+
+  def table_name
+    model_class.table_name
+  end
+end
+
+
+
+class BelongsToOptions < AssocOptions
+
+  def initialize(name, options = {})
+    if options.empty?
+      self.class.defaulter(name)
+    else
+      self.class.overrider(options)
+    end
+  end
+
+end
+
+class HasManyOptions < AssocOptions
+
+  def initialize(name, self_class_name, options = {})
+    if options.empty?
+      self.class.defaulter2(name, self_class_name)
+    else
+      self.class.overrider(options)
+    end
+  end
+
+end
+
+
+
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
