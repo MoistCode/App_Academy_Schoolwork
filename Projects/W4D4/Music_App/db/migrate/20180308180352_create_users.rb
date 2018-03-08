7dc7c4351ee9e3039a1e745d918a0af67eb59cd6
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-08 10:08:47 -0800

    Removed null and unique from index

--- Projects/W4D4/Music_App/db/migrate/20180308180352_create_users.rb
+++ Projects/W4D4/Music_App/db/migrate/20180308180352_create_users.rb
@@ -3,9 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :email, null: false, unique: true
       t.string :password_digest, null: false, unique: true
-      t.string :session_token
+      t.string :session_token, null: false, unique: true
       t.timestamps
     end
-    add_index :users, :session_token, null: false, unique: true
+    add_index :users, :session_token
   end
 end
