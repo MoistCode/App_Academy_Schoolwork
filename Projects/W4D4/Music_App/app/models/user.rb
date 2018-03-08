0731d0df89560d8c1464e0fb59bb96a732d66f90
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-08 10:11:10 -0800

    Added bcrypt gem

--- Projects/W4D4/Music_App/app/models/user.rb
+++ Projects/W4D4/Music_App/app/models/user.rb
@@ -1,2 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :email, :password_digest, :session_token, presnece: true
+  validates :password, presnece: { minimum: 6, allow_nil: true }
+  after_initialization: :ensure_session_token
+
+  def password=(password)
+    @password = password
+    BC
+  end
+
+  private
+
+  def ensure_session_token
+
+  end
 end
