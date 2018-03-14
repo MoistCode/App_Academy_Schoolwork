b26ba565681415922e2db3e3836bbcce4a23e047
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-14 16:26:07 -0700

    Oh god I actually got past p02

--- Projects/W5D3/Rails_Lite/bin/p01_basic_server.rb
+++ Projects/W5D3/Rails_Lite/bin/p01_basic_server.rb
@@ -1,1 +1,11 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
--- Projects/W5D3/Rails_Lite/lib/controller_base.rb
+++ Projects/W5D3/Rails_Lite/lib/controller_base.rb
@@ -2,31 +2,55 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
   def initialize(req, res)
+    @req, @res = req, res
+    @already_built ||= false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    return @already_built if @already_built
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise if already_built_response?
+    res['location'] = url
+    res.status = 302
+    @already_built = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.body = [content]
+    @already_built = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    raise if already_built_response?
+    class_name = self.class.to_s
+    class_name_capture = class_name.match(/(.+)Controller/)[1].downcase
+    path_capture = __FILE__.match(/(.+)lib/)[1]
+
+    final_path = "#{path_capture}/views/#{class_name_capture}_controller/#{template_name.to_s}.html.erb"
+
+    html_file = File.read(final_path)
+    rendered_html = ERB.new(html_file).result(binding)
+
+    render_content(rendered_html, 'text/html')
+
   end
 
   # method exposing a `Session` object
@@ -37,4 +61,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
