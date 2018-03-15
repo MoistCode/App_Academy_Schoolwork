2e0f377f48d164e2da8d94587fcc083dad82722e
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-15 12:12:30 -0700

    Completed range

---
+++ Projects/W5D4/Intro_JS_Exercises/phase_4_recursion.js
@@ -0,0 +1,53 @@
+// Write two versions; one of which utilizes recursion
+
+// Iterative versions
+
+function range(start, end) {
+  let num = start;
+  let finalArr = [];
+  while (num <= end) {
+    finalArr.push(num);
+    num++
+  }
+  return finalArr;
+}
+
+// console.log(range(1, 17));
+
+function sumRec(arr) {
+
+}
+
+function exponent(base, exp) {
+
+}
+
+function fibinacci(n) {
+
+}
+
+function deepDup(arr) {
+
+}
+
+// Recursive versions ========================================================
+
+function rangeRecursive(start, end) {
+
+}
+
+function sumRecRecursive(arr) {
+
+}
+
+function exponentRecursive(base, exp) {
+
+}
+
+function fibinacciRecursive(n) {
+
+}
+
+function deepDupRecursive(arr) {
+
+}
