f83a514db45bbe3a4b15c2978e26a93323f5784b
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-15 11:20:41 -0700

    Bubble sort works

--- Projects/W5D4/Intro_JS_Exercises/phase_3_iteration.js
+++ Projects/W5D4/Intro_JS_Exercises/phase_3_iteration.js
@@ -5,21 +5,24 @@ Array.prototype.bubbleSort = function(arr) {
   let arrCopy = arr.slice(0);
 
   while (unsorted) {
+    let swapped = false;
     for(i = 0; i < arr.length - 1; i++) {
       if (arrCopy[i] > arrCopy[i+1]) {
         var greaterNum = arrCopy[i];
         var lesserNum = arrCopy[i+1];
         arrCopy[i] = lesserNum;
         arrCopy[i+1] = greaterNum;
+        swapped = true;
       }
-      console.log(arrCopy);
     }
-
+    if (swapped === false) {
+      return arrCopy;
+    }
   }
 
 }
 
-newArr.bubbleSort([5,2,51,4,5,6,7,32,5,76,3,4,42,34,6,7,2]);
+console.log(newArr.bubbleSort([5,2,51,4,5,6,7,32,5,76,3,4,42,34,6,7,2]));
 
 String.prototype.substrings = function(str) {
 
