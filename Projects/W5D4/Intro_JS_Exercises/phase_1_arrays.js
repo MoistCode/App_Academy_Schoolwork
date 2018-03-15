eb78d98150c3567a5f91b63f4615d96b9ee0d03d
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-15 10:06:27 -0700

    uniq and twosum completed

--- Projects/W5D4/Intro_JS_Exercises/phase_1_arrays.js
+++ Projects/W5D4/Intro_JS_Exercises/phase_1_arrays.js
@@ -1,30 +1,40 @@
 Array.prototype.uniq = function(arr) {
   // Returns unique elements in the order that the came in
   let newArr = [];
-
   for(i = 0; i < arr.length; i++) {
     let unique = true;
-
     for(j = 0; j < newArr.length; j++) {
       if (arr[i] === newArr[j]) {
         unique = false;
       }
     }
-
     if (unique) {
       newArr.push(arr[i]);
     }
-
   }
-
   return newArr;
 }
+newArr = new Array;
+console.log(newArr.uniq([1,2,3,4,4,5,6,2,4,1,3,4,6,24]));
 
 Array.prototype.twoSum = function(arr) {
   // Returns an array of position pairs in which the sum === 0
-
+  let indexArr = [];
+  for(i = 0; i < arr.length; i++) {
+    for(j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        var posArr = [];
+        posArr.push(i);
+        posArr.push(j);
+        indexArr.push(posArr);
+      }
+    }
+  }
+  return indexArr
 }
 
-Array.prototype.transpose = function(arr1, arr2) {
+console.log(newArr.twoSum([1,2,3,6,2,4,-1,-1,-2,-4,-3,-2]));
 
+Array.prototype.transpose = function(arr1, arr2) {
+  
 }
