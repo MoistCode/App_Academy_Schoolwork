1916ed6ca12f992ce42b43b46bb207a58240b60d
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-15 10:30:12 -0700

    Working on myReduce

--- Projects/W5D4/Intro_JS_Exercises/phase_2_enumerable.js
+++ Projects/W5D4/Intro_JS_Exercises/phase_2_enumerable.js
@@ -1,11 +1,33 @@
-Array.prototype.myEach = function(arr, cb) {
+newArr = new Array;
+let testArr = [1,2,3,4,5,6,7];
 
+Array.prototype.myEach = function(arr, cb) {
+  for(i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+  }
+  return undefined;
 }
+
+let printNum = function(num) {
+  console.log(num)
+}
+
+newArr.myEach(testArr, printNum);
 
 Array.prototype.myMap = function(arr, cb) {
+  let arrCopy = [];
 
+  for(i = 0; i < arr.length; i ++) {
+    arrCopy.push(cb(arr[i]));
+  }
+
+  return arrCopy;
 }
 
-Array.prototype.myReduce = function(arr, cb) {
-  
+let timesTwo = function(num) {
+  return (num * 2);
 }
+
+console.log(newArr.myMap(testArr, timesTwo));
+
+Array.prototype.myReduce
