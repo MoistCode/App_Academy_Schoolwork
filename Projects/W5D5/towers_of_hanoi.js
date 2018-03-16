0e9de8f58da7cf305669d5a6a05de4b6a6af7c27
Tommy Pham <phamtommylong1991@gmail.com>
2018-03-16 14:00:44 -0700

    Planned out general tower of hanoi game

---
+++ Projects/W5D5/towers_of_hanoi.js
@@ -0,0 +1,15 @@
+// Main portion of the game
+
+class Game {
+  constructor() {
+    // This is ran when the game is started
+    // Creates the towers and disks
+    // Display the tower and disks
+    // We can have a while loop here that prompts until won
+      // Prompt user
+      // Update towers and disks
+        // Notify of any illegal moves
+      // Display towers and disks
+      // Repeat until won
+  }
+}
